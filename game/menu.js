57cdd285220bd4a0849c64605460267a19fa7ec8
Quinten Clause <quintenclause@hotmail.com>
2024-08-16 10:24:22 +0200

    Update menu.js

--- game/menu.js
+++ game/menu.js
@@ -9,7 +9,26 @@ menu.on('start', () => {
     });
 });
 
-let menuText = ft({text: 'Remove the (red) boats from\nThe Tridecomino Coral Reef.\nTo play: Tap!'});
+let intro = `
+The Tridecomino Coral Reef.
+This mysterious reef is constantly
+changing, but somehow the shape is
+always a polyomino of 13 squares.
+
+The many tourist boats are damaging
+the reef. It is your job to chase
+them away by gently tapping on them.
+
+Boats can only move back and forth.
+Red boats are above the reef.
+White boats are not above the reef.
+
+If there are no red boats left,
+the reef is safe.
+
+Good luck!`.
+
+let menuText = ft({text: intro});
 menu.on('draw', menuText.draw);
 
 export default menu;
