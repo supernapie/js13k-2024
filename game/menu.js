ec00176da3cadd0e31aa7658843a20be192b6013
quinten <quinten.clause@dynapps.be>
2024-08-19 15:23:49 +0200

    Clean up

--- game/menu.js
+++ game/menu.js
@@ -4,8 +4,6 @@ import d from './d.js';
 import path from './js/draw/path.js';
 import tappable from './js/pointer/rect.js';
 
-console.log(d);
-
 let menu = state();
 
 let boat = path({paths: [d.b], x: 0, y: 0, fills: ['Coral'], w: 40, h: 40});
@@ -17,8 +15,7 @@ menu.on('start', () => {
     });
 });
 
-let intro = `
-The Tridecomino Coral Reef.
+let intro = `The Tridecomino Coral Reef.
 This mysterious reef is constantly
 changing, but somehow the shape is
 always a polyomino of 13 squares.
@@ -40,10 +37,10 @@ Tap on the boat to start...`;
 
 let menuText = ft({text: intro});
 menu.on('draw', e => {
-menuText.draw(e);
-boat.x = menuText.x;
-boat.y = menuText.h + 40;
-boat.draw(e);
+    menuText.draw(e);
+    boat.x = menuText.x;
+    boat.y = menuText.h + 40;
+    boat.draw(e);
 });
 
 export default menu;
